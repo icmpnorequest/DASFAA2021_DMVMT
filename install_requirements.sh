1594969e99d41db7fc62e68f11d6f90d36817bc5
icmpnorequest <sculyt10@gmail.com>
2021-10-18 19:27:31 +0800

    Add requirements installation bash

---
+++ install_requirements.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pip3 install torch
+pip3 install torchtext
+pip3 install spacy
+python3 -m spacy download en_core_web_sm
+pip3 install sklearn
+pip3 install seaborn
+pip3 install matplotlib
